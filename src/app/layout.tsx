7618274ddeda685b241b56fb61ffc7c4d57e36b0
jonnyo74 <john@doyouneedahome.com>
2026-06-05 11:36:36 -0400

    Add GA4 measurement ID G-24154P036M via GoogleTagManager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Playfair_Display, Inter } from 'next/font/google'
+import { GoogleTagManager } from '@next/third-parties/google'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import './globals.css'
@@ -39,6 +40,7 @@ export default function RootLayout({
         <div className="flex-1">{children}</div>
         <Footer />
       </body>
+      <GoogleTagManager gtmId="G-24154P036M" />
     </html>
   )
 }
